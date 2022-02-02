57503e1e983a7c50eea8c43cafd8ef73a1ecf7dd
Ilolo Izu <iloloizu97@gmail.com>
2022-02-02 10:49:32 -0600

    commit tree bcbbad9ce364c97611ec406b0945620ad8725913

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,40 @@
 # your code goes here
+
+class Person
+
+    attr_accesser :name
+    attr_reader: :bank_account, :happiness, :hygiene
+    
+
+    def initialize(name, bank_account=25, happiness=8, hygiene=8)
+        @name = name
+        @bank_account = bank_account
+        @happiness = happiness
+        @hygiene = hygiene
+    end
+
+    # def bank_account
+    #     @bank_account
+    # end
+
+    def bank_account=(value)
+        @bank_account = value
+    end
+
+    # def happiness 
+    #     @happiness
+    # end 
+
+    def happiness=(value)
+        @happiness = value
+    end
+    
+    # def hygiene
+    #     @hygiene
+    # end
+
+    def hygiene=(value)
+        @hygiene = value
+    end
+
+end
